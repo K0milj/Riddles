2aeda87907c1f59881c3da976b1125315ddb37a8
Luka Miljković <92792328+K0milj@users.noreply.github.com>
2022-01-01 18:26:55 +0100

    Add files via upload

--- Riddles/app.js
+++ Riddles/app.js
@@ -6,17 +6,21 @@ if (username == "" || username == null) {
 } else {
     nameOfUser.textContent = `Hello ${username}!`;
 }
+var scoreBox = document.createElement("p");
+scoreBox.innerHTML = `Score: ${0}`;
 userBox.append(nameOfUser);
+userBox.append(scoreBox);
 userBox.id = `userBox`;
 document.getElementById("body").append(userBox);
 
 var pravila = document.createElement("div");
+pravila.id = "pravila";
 pravila.innerHTML =
     `   
         <h3>Rules:</h3>
         <ul>
-            <li>Answer only with one word,</li>
-            <li>Begin word with a capital letter and</li>
+            <li>Answer only with one word if you can,</li>
+            <li>Begin answer with a capital letter and</li>
             <li>Don't cheat 😉</li>
         </ul>
     `
@@ -28,20 +32,28 @@ const pitanja = [
     "What kind of goose fights with snakes?",
     "You answer me, although I never ask you questions. What am I?",
     "I am wet when drying. What am I?",
-    "What word is always pronounced wrong?"
+    "What word is always pronounced wrong?",
+    "David's father has three sons: Snap, Crackle, and _____?",
+    "What room do ghosts avoid?",
+    "What runs around the whole yard without moving?"
 ];
 
 const odgovori = [
-    "Noon",
+    "NOON",
     "Two",
     "Mongoose",
     "Telephone",
     "Towel",
-    "Wrong"
+    "Wrong",
+    "David",
+    "Living room",
+    "Fence"
 ];
 
 for (var i = 0; i < pitanja.length; i++) {
     var pitanjeBox = document.createElement("div");
+    pitanjeBox.classList.add("hideQuestion");
+    pitanjeBox.id = `pitanjeBox${i}`;
     pitanjeBox.innerHTML += `<h4>Question ${i + 1}: ${pitanja[i]}</h4>`;
     var poruka = document.createElement("p");
     pitanjeBox.append(poruka);
@@ -50,13 +62,19 @@ for (var i = 0; i < pitanja.length; i++) {
     document.getElementById("body").append(pitanjeBox);
     pitanjeBox.innerHTML +=
         `
+        <form autocomplete="off" id=form${i}>
         <input type="text" name="odgovor${i}" id="odgovor${i}">
-        <button onclick="myFunction()">Answer</button>
+        <button onclick="myFunction()">Submit</button>
+        </form>
         <br>
     `;
 
+    document.getElementById(`form${i}`).addEventListener("submit", e => {
+        e.preventDefault();
+    })
+
     var resenje = document.createElement("p");
-    resenje.innerText = `${odgovori[i]}`;
+    resenje.innerText = `Answer ${i + 1}: ${odgovori[i]}`;
     pitanjeBox.append(resenje);
     resenje.classList.add("resenjeHide");
     resenje.id = `resenje${i}`;
@@ -65,6 +83,11 @@ for (var i = 0; i < pitanja.length; i++) {
         for (var i = 0; i < pitanja.length; i++) {
             if (document.getElementById(`odgovor${i}`).value === odgovori[i]) {
                 document.getElementById(`pitanje${i}`).innerHTML = `<p>Correct!</p>`;
+                scoreBox.innerHTML = `Score: ${i + 1}`;
+                document.getElementById(`pitanjeBox${i + 1}`).classList.remove("hideQuestion");
+                document.getElementById(`pitanjeBox${i + 1}`).classList.add("showQuestion");
+                document.getElementById(`odgovor${i}`).setAttribute("disabled", "");
+                window.scrollTo(0, window.innerHeight + 50);
             } else if (document.getElementById(`odgovor${i}`).value === "") {
                 document.getElementById(`pitanje${i}`).innerHTML = ``;
             } else if (document.getElementById(`odgovor${i}`).value !== odgovori[i]) {
@@ -74,13 +97,12 @@ for (var i = 0; i < pitanja.length; i++) {
     }
 }
 
-
-
 function showAnswers() {
     for (var i = 0; i < pitanja.length; i++) {
         if (document.getElementById(`resenje${i}`).classList.contains("resenjeHide")) {
             document.getElementById(`resenje${i}`).classList.remove("resenjeHide");
             document.getElementById(`resenje${i}`).classList.add("resenjeShow");
+            scoreBox.innerHTML = `Score: 0`;
         } else if (document.getElementById(`resenje${i}`).classList.contains("resenjeShow")) {
             document.getElementById(`resenje${i}`).classList.remove("resenjeShow");
             document.getElementById(`resenje${i}`).classList.add("resenjeHide");
